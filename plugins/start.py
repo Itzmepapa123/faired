cad0d62ba1ddc257d8dbbfdbf6acd9c810472fde
Itzmepapa123 <absorbinghunger@gmail.com>
2024-06-24 07:40:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -73,17 +73,12 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:
-    # Check if the message exists before trying to copy it
-                if msg:
-                    await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
-                    await asyncio.sleep(0.5)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-    # Retry the copy operation only if the message still exists
-                if msg:
-                    await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup, protect_content=PROTECT_CONTENT)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
             except:
-    # Silently pass for all other exceptions
                 pass
         return
     else:
