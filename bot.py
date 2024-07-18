8c1412b1e2d70bd9e101c9ab6a900eac846b340e
Itzmepapa123 <absorbinghunger@gmail.com>
2024-07-18 19:35:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,37 +27,8 @@ AUTHORIZED_USERS = [7030439873, 987654321]  # Replace with actual user IDs
 UPSTREAM_REPO_URL = "https://github.com/Itzmepapa123/faired.git"
 UPSTREAM_BRANCH = "Updated"
 
-try:
-    if len(UPSTREAM_REPO) == 0:
-       raise TypeError
-except:
-    UPSTREAM_REPO = None
-
-try:
-    if len(UPSTREAM_BRANCH) == 0:
-       raise TypeError
-except:
-    UPSTREAM_BRANCH = 'master'
 
 # Function to initialize and update from upstream repository
-def initialize_and_update_repo():
-    if UPSTREAM_REPO is not None:
-        if os.path.exists('.git'):
-            subprocess.run(["rm", "-rf", ".git"])
-
-        update = subprocess.run([f"git init -q \
-                         && git config --global user.email yashoswal18@gmail.com \
-                         && git config --global user.name mergebot \
-                         && git add . \
-                         && git commit -sm update -q \
-                         && git remote add origin {UPSTREAM_REPO} \
-                         && git fetch origin -q \
-                         && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
-
-        if update.returncode == 0:
-            print("Repository initialized and updated successfully.")
-        else:
-            print("Failed to initialize or update repository.")
 
 def restart_bot():
     os.execv(sys.executable, ['python'] + sys.argv)
@@ -75,6 +46,39 @@ class Bot(Client):
             bot_token=TG_BOT_TOKEN
         )
         self.LOGGER = LOGGER
+
+    try:
+       if len(UPSTREAM_REPO) == 0:
+           raise TypeError
+       except:
+           UPSTREAM_REPO = None
+
+    try:
+        if len(UPSTREAM_BRANCH) == 0:
+           raise TypeError
+    except:
+        UPSTREAM_BRANCH = 'master'
+
+    async def initialize_and_update_repo():
+        if UPSTREAM_REPO is not None:
+        if os.path.exists('.git'):
+            subprocess.run(["rm", "-rf", ".git"])
+
+        update = subprocess.run([f"git init -q \
+                         && git config --global user.email yashoswal18@gmail.com \
+                         && git config --global user.name mergebot \
+                         && git add . \
+                         && git commit -sm update -q \
+                         && git remote add origin {UPSTREAM_REPO} \
+                         && git fetch origin -q \
+                         && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+
+        if update.returncode == 0:
+            print("Repository initialized and updated successfully.")
+        else:
+            print("Failed to initialize or update repository.")
+            
+    
 
     async def send_started_message(self, user_id: int):
         try:
