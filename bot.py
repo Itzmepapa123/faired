97b708a6049befc7e4d40be8a479abaef9b0280e
Itzmepapa123 <absorbinghunger@gmail.com>
2024-07-18 19:07:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,12 +27,37 @@ AUTHORIZED_USERS = [7030439873, 987654321]  # Replace with actual user IDs
 UPSTREAM_REPO_URL = "https://github.com/Itzmepapa123/faired.git"
 UPSTREAM_BRANCH = "Updated"
 
-def update_from_upstream():
-    repo = git.Repo(os.getcwd())
-    origin = repo.remotes.origin
-    origin.set_url(UPSTREAM_REPO_URL)
-    origin.fetch()
-    origin.pull(f'origin/{UPSTREAM_BRANCH}')
+try:
+    if len(UPSTREAM_REPO) == 0:
+       raise TypeError
+except:
+    UPSTREAM_REPO = None
+
+try:
+    if len(UPSTREAM_BRANCH) == 0:
+       raise TypeError
+except:
+    UPSTREAM_BRANCH = 'master'
+
+# Function to initialize and update from upstream repository
+def initialize_and_update_repo():
+    if UPSTREAM_REPO is not None:
+        if os.path.exists('.git'):
+            subprocess.run(["rm", "-rf", ".git"])
+
+        update = subprocess.run([f"git init -q \
+                         && git config --global user.email yashoswal18@gmail.com \
+                         && git config --global user.name mergebot \
+                         && git add . \
+                         && git commit -sm update -q \
+                         && git remote add origin {UPSTREAM_REPO} \
+                         && git fetch origin -q \
+                         && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+
+        if update.returncode == 0:
+            print("Repository initialized and updated successfully.")
+        else:
+            print("Failed to initialize or update repository.")
 
 def restart_bot():
     os.execv(sys.executable, ['python'] + sys.argv)
